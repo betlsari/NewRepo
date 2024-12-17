cd6c944d613f362dc0d6f75da515c3c1f8f71884
Betül Sarı <betlsari07@gmail.com>
2024-12-17 16:27:05 +0300

    form1

--- Console/Program.cs
+++ Console/Program.cs
@@ -16,7 +16,7 @@ namespace Console
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Forum());
+            Application.Run(new Form1());
         }
     }
 }
